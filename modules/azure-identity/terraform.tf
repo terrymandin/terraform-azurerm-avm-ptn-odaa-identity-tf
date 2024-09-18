1dff3c28e2b12d6a94b90e9608438f1d2df674f2
Terry Mandin <terry.mandin@microsoft.com>
2024-09-18 14:24:48 -0600

    Fix Linting errors

--- modules/azure-identity/terraform.tf
+++ modules/azure-identity/terraform.tf
@@ -1,6 +1,10 @@
 terraform {
   required_version = "~> 1.5"
   required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.53.1"
+    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.71"
